eb64dd347ee28d1b76c6cdbb113cfb3d1aa4c093
Salaheddine Namir <71770514+T3rryc@users.noreply.github.com>
2023-05-25 14:24:49 +0200

    Update ArraySorts.cs

--- AlgorithmsTestProject/ArraySorts.cs
+++ AlgorithmsTestProject/ArraySorts.cs
@@ -18,16 +18,119 @@
 
         public static void MySort2(int[] array)
         {
+           // insertion sort
+            for (var i = 1; i < array.Length; i++)
+            {
+                var j = i;
+                while (j > 0 && array[j] < array[j - 1])
+                {
+                    ArrayProblems.Swap(array, j, j - 1);
+                    j--;
+                }
+            }
+
         }
 
         public static void MergeSort(int[] array)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (array.Length < 2)
+            {
+                return;
+            }
+            var mid = array.Length / 2;
+            var left = new int[mid];
+            var right = new int[array.Length - mid];
+            for (var i = 0; i < mid; i++)
+            {
+                left[i] = array[i];
+            }
+            for (var i = mid; i < array.Length; i++)
+            {
+                right[i - mid] = array[i];
+            }
+            MergeSort(left);
+            MergeSort(right);
+            Merge(array, left, right);
+
+           
+              
+
+            
+
+        }
+
+        public static void Merge(int[] array, int[] left, int[] right)
+        {
+            var i = 0;
+            var j = 0;
+            var k = 0;
+            while (i < left.Length && j < right.Length)
+            {
+                if (left[i] <= right[j])
+                {
+                    array[k] = left[i];
+                    i++;
+                    k++;
+                }
+                else
+                {
+                    array[k] = right[j];
+                    j++;
+                    k++;
+                }
+            }
+            while (i < left.Length)
+            {
+                array[k] = left[i];
+                i++;
+                k++;
+            }
+            while (j < right.Length)
+            {
+                array[k] = right[j];
+                j++;
+                k++;
+            }
         }
 
         public static void HeapSort(int[] array)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            BuildHeap(array);
+            for (var i = array.Length - 1; i >= 1; i--)
+            {
+                ArrayProblems.Swap(array, 0, i);
+                Heapify(array, i, 0);
+            }
+
+        }
+        public static void BuildHeap(int[] array)
+        {
+            for (var i = array.Length / 2 - 1; i >= 0; i--)
+            {
+                Heapify(array, array.Length, i);
+            }
+        }
+
+        public static void Heapify(int[] array, int n, int i)
+        {
+            var largest = i;
+            var left = 2 * i + 1;
+            var right = 2 * i + 2;
+            if (left < n && array[left] > array[largest])
+            {
+                largest = left;
+            }
+            if (right < n && array[right] > array[largest])
+            {
+                largest = right;
+            }
+            if (largest != i)
+            {
+                ArrayProblems.Swap(array, i, largest);
+                Heapify(array, n, largest);
+            }
         }
 
         public static void BubbleSort(int[] array)
@@ -52,36 +155,158 @@
 
         public static void ShuffleSort(int[] array)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            // cocktail shatter sort
+            var i = 0;
+            var j = array.Length - 1;
+            while (i < j)
+            {
+                if (array[i] > array[i + 1])
+               {
+                    ArrayProblems.Swap(array, i, i + 1);
+               }
+               i++;
+                if (array[j] < array[j - 1])
+               {
+                   ArrayProblems.Swap(array, j, j - 1);
+                }
+               j--;
+            }
         }
 
         public static void IntroSort(int[] array)
         {
             throw new NotImplementedException();
+            
+
         }
+         
+       
+        
 
         public static void CocktailSort(int[] array)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var swapped = false;
+            var n = array.Length;
+            do
+            {
+                for (var i = 1; i < n; ++i)
+                {
+                    if (array[i - 1] > array[i])
+                    {
+                        ArrayProblems.Swap(array, i - 1, i);
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                {
+                    break;
+                }
+                swapped = false;
+                --n;
+                for (var i = n - 1; i > 0; --i)
+                {
+                    if (array[i - 1] > array[i])
+                    {
+                        ArrayProblems.Swap(array, i - 1, i);
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
         }
 
         public static void QuickSort(int[] array)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            
+
+
+
+
+              
         }
+     
+          
 
         public static void BlockSort(int[] array)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var n = array.Length;
+            var max = array[0];
+            var min = array[0];
+            for (var i = 1; i < n; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            var range = max - min + 1;
+            var count = new int[range];
+            for (var i = 0; i < n; i++)
+            {
+                count[array[i] - min]++;
+            }
+            for (var i = 1; i < range; i++)
+            {
+                count[i] += count[i - 1];
+            }
+            var output = new int[n];
+            for (var i = n - 1; i >= 0; i--)
+            {
+                output[count[array[i] - min] - 1] = array[i];
+                count[array[i] - min]--;
+            }
+            for (var i = 0; i < n; i++)
+            {
+                array[i] = output[i];
+            }
+
         }
 
         public static void BogoSort(int[] array)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var n = array.Length;
+            while (!IsSorted(array))
+            {
+                Shuffle(array);
+            }
+
         }
+            public static bool IsSorted(int[] array)
+        {
+            for (var i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] > array[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+            }
+
+        public static void Shuffle(int[] array)
+        {
+            var n = array.Length;
+            for (var i = 0; i < n; i++)
+            {
+                var r = i + (int)(new Random().NextDouble() * (n - i));
+                ArrayProblems.Swap(array, r, i);
+            }
+        }
+       
+
 
         public static void DoNothingSort(int[] array)
         {
+
         }
 
         public static void EvilSort(int[] array)
